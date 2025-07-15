aa098824bc7a91eb7f43e35aad43f24844f781a8
Gabriel Marrero <gab.bmar.05@gmail.com>
2025-07-15 12:24:09 -0300

    agrego rutas para la navegación en la aplicación. implemento servicio de toast para mostrar mensajes temporales.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
+import { CustomerFormComponent } from './components/customer-form/customer-form.component';
+import { WorkoutsComponent } from './pages/workouts/workouts.component';
+import { CustomersComponent } from './pages/customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'clientes', component: CustomersComponent },
+	{ path: 'clientes/nuevo', component: CustomerFormComponent },
+	{ path: 'clientes/:id', component: CustomerFormComponent },
+	{ path: 'workouts', component: WorkoutsComponent },
+	{ path: 'workouts/new', component: WorkoutFormComponent },
+	{ path: 'workouts/:id', component: WorkoutFormComponent },
+	{ path: 'sobre', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastService } from './services/toast.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'brewery';
+export class AppComponent implements OnInit {
+  title = 'FitAdmin App';
+  showToast = false;
+  mensajeToast = '';
+
+  constructor(private toastService: ToastService) {}
+
+  ngOnInit(): void {
+      this.toastService.toast$.subscribe(msj => {
+        this.mensajeToast = msj;
+        this.showToast = true;
+        setTimeout(() => this.showToast = false, 3000);
+      })
+  }
+
+  cerrarToast() { this.showToast = false; }
 }
