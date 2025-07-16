e1a78f4bb331a65fdf209a84d94d3441bc700c6d
Gabriel Marrero <gab.bmar.05@gmail.com>
2025-07-16 14:00:30 -0300

    agrego componente de lista de clientes con funcionalidad de carga, eliminación y manejo de errores

---
+++ src/app/components/customer-list/customer-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../models/customer.model';
+import { CustomerService } from '../../services/customer.service';
+import { ToastService } from '../../services/toast.service';
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrl: './customer-list.component.scss'
+})
+export class CustomerListComponent implements OnInit {
+  customers: Customer[] = [];
+  loading = true;
+
+  constructor (
+    private customerService: CustomerService,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit(): void {
+      this.loadCustomers();
+  }
+
+  loadCustomers(): void {
+    this.customerService.getAll().subscribe({
+      next: (data) => {
+        this.customers = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.toastService.showToast('Error al cargar clientes');
+        this.loading = false;
+      }
+    });
+  }
+
+  eliminarCliente(id: string): void {
+    if (!confirm('¿Estás seguro de que deseas eliminar este cliente?')) return;
+
+    this.customerService.delete(id).subscribe(() => {
+      this.toastService.showToast('Cliente eliminado');
+      this.customers = this.customers.filter(c => c.id !== id);
+    });
+  }
+}
