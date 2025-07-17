54272167afd5aae411f027393f71cdb456dadf52
Gabriel Marrero <gab.bmar.05@gmail.com>
2025-07-17 13:51:41 -0300

    agrego componente de selección de ejercicios y formulario de entrenamiento con validaciones y manejo de datos

---
+++ src/app/components/exercise-picker/exercise-picker.component.ts
@@ -0,0 +1,73 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Exercise } from '../../models/exercise.model';
+import { ExerciseService } from '../../services/exercise.service';
+
+@Component({
+  selector: 'app-exercise-picker',
+  templateUrl: './exercise-picker.component.html',
+  styleUrl: './exercise-picker.component.scss'
+})
+export class ExercisePickerComponent implements OnInit {
+  exercises: Exercise[] = [];
+  filtered: Exercise[] = [];
+  search = '';
+
+  currentPage = 1;
+  pageSize = 10;
+
+  imgAPIUrl = 'https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/';
+  imageIndexMap: { [id: string]: number } = {};
+
+  @Output() onAddExercise = new EventEmitter<Exercise>();
+
+  constructor(private exerciseService: ExerciseService) {}
+
+  ngOnInit(): void {
+      this.exerciseService.getAll().subscribe(data => {
+        this.exercises = data;
+        this.filtrar();
+      });
+  }
+
+  filtrar(): void {
+    const query = this.search.toLowerCase();
+    this.filtered = this.exercises.filter(
+      e => e.name.toLowerCase().includes(query)
+    )
+    this.currentPage = 1;
+  }
+
+  seleccionar(ejercicio: Exercise): void {
+    this.onAddExercise.emit(ejercicio);
+  }
+
+  get pagedExercises(): Exercise[] {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return this.filtered.slice(start, start + this.pageSize);
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.filtered.length / this.pageSize);
+  }
+
+  cambiarPagina(direccion: 'prev' | 'next'): void {
+    if (direccion === 'prev' && this.currentPage > 1) this.currentPage--;
+    if (direccion === 'next' && this.currentPage < this.totalPages()) this.currentPage++;
+  }
+
+  alternarImagen(id: string): void {
+    const actual = this.imageIndexMap[id] ?? 0;
+    this.imageIndexMap[id] = actual === 0 ? 1 : 0;
+  }
+
+  obtenerImagen(e: Exercise): string {
+    const index = this.imageIndexMap[e.id] ?? 0;
+    const path = e.images?.[index];
+
+    if (!path) return 'https://uning.es/inicio/image-placeholder/';
+
+    if (path.startsWith('http')) return path;
+
+    return `${this.imgAPIUrl}${path}`;
+  }
+}
---
+++ src/app/components/workout-form/workout-form.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../models/customer.model';
+import { Workout, WorkoutExercise } from '../../models/workout.model';
+import { CustomerService } from '../../services/customer.service';
+import { WorkoutService } from '../../services/workout.service';
+import { ToastService } from '../../services/toast.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Exercise } from '../../models/exercise.model';
+
+@Component({
+  selector: 'app-workout-form',
+  templateUrl: './workout-form.component.html',
+  styleUrl: './workout-form.component.scss'
+})
+export class WorkoutFormComponent implements OnInit {
+  customers: Customer[] = [];
+  selectedCustomerId = '';
+  nombre = '';
+  ejercicios: WorkoutExercise[] = [];
+
+  workoutId?: string;
+  editMode = false;
+
+  constructor(
+    private customerService: CustomerService,
+    private workoutService: WorkoutService,
+    private toastService: ToastService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+      this.customerService.getAll().subscribe(data => {
+        this.customers = data;
+      });
+
+      this.workoutId = this.route.snapshot.paramMap.get('id') ?? undefined;
+      if (this.workoutId) {
+        this.editMode = true;
+
+        this.workoutService.getById(this.workoutId).subscribe(workout => {
+          this.nombre = workout.nombre;
+          this.selectedCustomerId = workout.customerId;
+          this.ejercicios = workout.ejercicios;
+        });
+      }
+  }
+
+  agregarEjercicio(e: Exercise): void {
+    if (this.ejercicios.length >= 10) {
+      this.toastService.showToast('Máximo 10 ejercicios por entrenamiento');
+      return;
+    }
+
+    const yaExiste = this.ejercicios.find(ej => ej.ejercicioId === e.id);
+    if (yaExiste) {
+      this.toastService.showToast('El ejercicio seleccionado ya existe');
+      return;
+    }
+    
+    this.ejercicios.push({
+      ejercicioId: e.id,
+      nombre: e.name,
+      series: 3,
+      repeticiones: 10
+    });
+  }
+
+  eliminarEjercicio(id: string): void {
+    this.ejercicios = this.ejercicios.filter(e => e.ejercicioId !== id);
+  }
+
+  guardar(): void {
+    if (!this.selectedCustomerId || !this.nombre || this.ejercicios.length === 0) {
+      this.toastService.showToast('Completa todos los campos');
+      return;
+    }
+    
+    const workout: Workout = {
+      id: this.workoutId,
+      customerId: this.selectedCustomerId,
+      nombre: this.nombre,
+      fecha: new Date().toISOString(),
+      ejercicios: this.ejercicios
+    };
+
+    if (this.editMode && this.workoutId){
+      this.workoutService.update(workout).subscribe(() => {
+        this.toastService.showToast('Entrenamiento actualizado');
+        this.router.navigate(['/workouts']);
+      });
+    } else {
+      this.workoutService.create(workout).subscribe(() => {
+        this.toastService.showToast('Entrenamiento creado');
+        this.router.navigate(['/workouts']);
+      });
+    }
+
+  }
+
+  validarRango(e: WorkoutExercise, campo: 'series' | 'repeticiones', min: number, max: number): void {
+    const valor = e[campo];
+    if (valor < min) e[campo] = min;
+    if (valor > max) e[campo] = max;
+  }
+}
